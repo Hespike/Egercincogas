99a32897dfa1289e60c66743397850d2454e2a56
Sárdi András <sardi.andras@gmail.com>
2021-04-12 10:43:03 +0200

    Create Eger.java

---
+++ Eger.java
@@ -0,0 +1,13 @@
+public class Eger extends Allat {
+
+    private int cincogasiMertek;
+
+    public Eger(int meret, int cincogasiMertek) {
+        super("eger", meret);
+        this.cincogasiMertek = cincogasiMertek;
+    }
+@Override
+    public String toString(){
+        return super.toString() + " " + cincogasiMertek;
+    }
+}
